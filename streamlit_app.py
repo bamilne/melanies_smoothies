bba329d2db4e4b15d73270d6f4689e427dd9fb8f
bamilne <bamilne24@gmail.com>
2025-08-05 14:35:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,13 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 #st.stop()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
